9d93ae2edd492f3b16e8d9daee152517e4ca082f
Austin Nze <109114297+Ausranking@users.noreply.github.com>
2022-09-17 01:02:45 +0100

    Add files via upload

---
+++ order-summary-component-main/index.js
@@ -0,0 +1,18 @@
+const planSub = document.getElementById("jsaction");
+
+planSub.addEventListener("click", () => {
+  planSub.innerHTML = "<h4>Processing...</h4>"
+  planSub.style.color="hsl(255, 77%, 57%)"
+  planSub.style.background="none"
+});
+
+var cancelSub = document.getElementById("return");
+
+cancelSub.addEventListener("click", () => {
+  cancelSub.innerHTML = "<h3>You dey mad?</h3>";
+  if (confirm("You dey Mad?")) {
+    location.href = "#";
+  }
+});
+
+// alert(location.href)
